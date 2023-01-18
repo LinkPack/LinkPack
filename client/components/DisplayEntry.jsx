2ddc02deecfb2b3124eddc46f73bfc0ce6d7578e
kjdooley1 <110135934+kjdooley1@users.noreply.github.com>
2023-01-18 14:18:00 -0500

    links now redirect to correct page

--- client/components/DisplayEntry.jsx
+++ client/components/DisplayEntry.jsx
@@ -5,7 +5,7 @@ function DisplayEntry({link, label}) {
   return(
     <div>
       <div>{label}</div>
-      <a href={link}>{link}</a>
+      <a href={'//' + link} target='_blank'>{link}</a>
     </div>
   ); 
 }
