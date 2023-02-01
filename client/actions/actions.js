a1cac363e7e1895c36608ce44e92b135e3e30aa2
kjdooley1 <110135934+kjdooley1@users.noreply.github.com>
2023-02-01 14:02:55 -0500

    initial ts additions

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,26 +1,53 @@
-import * as types from '../constants/actionTypes';
-
-export const addEntryActionCreator = (label, link, keyId) => ({
-  type: types.ADD_ENTRY,
-  payload: {
-    label,
-    link,
-    keyId
-  }
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
 });
-
-export const generateLinkPackActionCreator = () => ({
-  type: types.GENERATE_LINKPACK
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteEntryActionCreator = exports.getEntriesActionCreator = exports.generateLinkPackActionCreator = exports.addEntryActionCreator = void 0;
+const types = __importStar(require("../constants/actionTypes.js"));
+const addEntryActionCreator = (label, link, keyId) => ({
+    type: types.ADD_ENTRY,
+    payload: {
+        label,
+        link,
+        keyId
+    }
 });
-
-export const getEntriesActionCreator = (receivedEntries) => ({
-  type: types.GET_ENTRIES,
-  payload: {
-    receivedEntries
-  }
-})
-export const deleteEntryActionCreator = (keyId) => ({
-  type: types.DELETE_ENTRY,
-  payload: keyId
+exports.addEntryActionCreator = addEntryActionCreator;
+const generateLinkPackActionCreator = () => ({
+    type: types.GENERATE_LINKPACK
 });
-
+exports.generateLinkPackActionCreator = generateLinkPackActionCreator;
+const getEntriesActionCreator = (receivedEntries) => ({
+    type: types.GET_ENTRIES,
+    payload: {
+        receivedEntries
+    }
+});
+exports.getEntriesActionCreator = getEntriesActionCreator;
+const deleteEntryActionCreator = (keyId) => ({
+    type: types.DELETE_ENTRY,
+    payload: keyId
+});
+exports.deleteEntryActionCreator = deleteEntryActionCreator;
+//# sourceMappingURL=actions.js.map
--- client/constants/actionTypes.js
+++ client/constants/actionTypes.js
@@ -1,5 +1,9 @@
-export const EXAMPLE = 'EXAMPLE';
-export const ADD_ENTRY = 'ADD_ENTRY';
-export const GENERATE_LINKPACK = 'GENERATE_LINKPACK';
-export const GET_ENTRIES = 'GET_ENTRIES';
-export const DELETE_ENTRY = 'DELETE_ENTRY';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DELETE_ENTRY = exports.GET_ENTRIES = exports.GENERATE_LINKPACK = exports.ADD_ENTRY = exports.EXAMPLE = void 0;
+exports.EXAMPLE = 'EXAMPLE';
+exports.ADD_ENTRY = 'ADD_ENTRY';
+exports.GENERATE_LINKPACK = 'GENERATE_LINKPACK';
+exports.GET_ENTRIES = 'GET_ENTRIES';
+exports.DELETE_ENTRY = 'DELETE_ENTRY';
+//# sourceMappingURL=actionTypes.js.map
